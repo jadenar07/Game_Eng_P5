1431239182624856dd77195726b414d27d9f4d6d
Jaden Ritchie <147543860+jadenar07@users.noreply.github.com>
2024-11-09 21:55:04 -0500

    Update main.cpp

--- SDLSimple/main.cpp
+++ SDLSimple/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Jaden Ritchie
+* Assignment: Rise of the AI
+* Date due: 2024-11-9, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define LOG(argument) std::cout << argument << '\n'
